13bfef4f7cad84fda8c2a1cf443cc3d4d19d71ef
padartamas <padar@netsurfclub.hu>
2026-01-29 00:10:52 +0100

    correct service project install.sh

--- MndpTray/MndpService.Core/Install/install.sh
+++ MndpTray/MndpService.Core/Install/install.sh
@@ -15,8 +15,8 @@ SERVICE_VERSION="2.3.0"
 IS_ACTIVE=$(sudo systemctl is-active $SERVICE_NAME)
 if [ "$IS_ACTIVE" == "active" ]; then
 
-Write-Host "Service is running, stopping it..."
-Write-Host
+echo "Service is running, stopping it..."
+echo
 
 systemctl disable $SERVICE_NAME
 systemctl stop $SERVICE_NAME
@@ -24,18 +24,18 @@ systemctl status $SERVICE_NAME
 
 fi
 
-Write-Host
-Write-Host "Downloading service..."
-Write-Host
+echo
+echo "Downloading service..."
+echo
 
 mkdir -p $PROGRAM_PATH
 wget -O $PROGRAM_PATH/$PROGRAM_NAME https://github.com/xmegz/MndpTray/releases/download/v$SERVICE_VERSION/MndpService.Core.Full
 chmod +x $PROGRAM_PATH/$PROGRAM_NAME
 
 
-Write-Host
-Write-Host "Creating systemd service..."
-Write-Host
+echo
+echo "Creating systemd service..."
+echo
 
 cat > /etc/systemd/system/mndp.service << EOF
 [Unit]
@@ -59,9 +59,9 @@ WantedBy=multi-user.target
 EOF
 
 
-Write-Host
-Write-Host "Starting systemd service..."
-Write-Host
+echo
+echo "Starting systemd service..."
+echo
 
 systemctl daemon-reload
 systemctl enable $SERVICE_NAME
